b02696ab1674707dc7ab9411e47c1ac223a7c1d9
Paul Grad <paulgrad3rd@gmail.com>
2016-07-08 12:12:56 -0700

    Changed Version Control and Asset Serialization Modes
    The previous settings appear to produce corrupted projects when I clone
    from github on other computers. I’m switching to these settings so that
    I can work on this project from other computers.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour, Mover {
+	public GameObject target;
+	private NavMeshAgent nvAgent;
+	private bool stopped = false;
+	private Vector3 terrainBounds;
+
+	// Use this for initialization
+	void Start () {
+		nvAgent = GetComponent<NavMeshAgent> ();
+		EventManager.RegisterEvent ("StartPlayerMove", startMoving);
+		EventManager.RegisterEvent ("Move", getMoving);
+		EventManager.RegisterEvent ("Stop", stopMoving);
+	}
+
+	public void startMoving () {
+		terrainBounds = GameObject.FindGameObjectWithTag ("Level").GetComponent<Terrain> ().terrainData.size;
+		terrainBounds.x /= 2;
+		getMoving ();
+	}
+
+	public void getMoving () {
+		if(stopped)
+			nvAgent.Resume ();
+		else
+			nvAgent.SetDestination (target.transform.position);
+	}
+
+	public void stopMoving () {
+		nvAgent.Stop ();
+		stopped = true;
+	}
+}
